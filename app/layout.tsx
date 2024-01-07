f001a34e2d281eea2e4056b359a803ec6b3b4d36
sajawalhassan <sajawalhassan.1feb@gmail.com>
2024-01-07 19:26:10 +0500

    Add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,30 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Sajawal Hassan",
-  description: "My portfolio",
+  description: "Sajawal Hassan's portfolio, a Data Scientist and a Fullstack Web Developer",
   authors: { name: "Sajawal Hassan", url: "https://sajawalhassan.vercel.app" },
+  keywords: [
+    "Data",
+    "Data Scientist",
+    "Websites",
+    "Web Developer",
+    "Fullstack Developer",
+    "Frontend Developer",
+    "Backend Developer",
+    "Sajawal Hassan",
+    "Porfolio",
+    "Personal Portfolio",
+  ],
+  category: "Portfolio",
+  creator: "Sajawal Hassan",
+  abstract: "Sajawal Hassan's portfolio, a Data Scientist and a Fullstack Web Developer",
+  classification: "Sajawal Hassan portfolio",
+  publisher: "vercel",
+  appLinks: {
+    web: {
+      url: "https://reddit-clone-nextjs-1f12.vercel.app",
+    },
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
